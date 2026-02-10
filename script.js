205b69fcf886770338986d43464cd2170e765731
varshinilokesh323-eng <varshinilokesh323@gmail.com>
2026-02-10 20:32:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// Optional JS smooth scrolling for older browsers
+const links = document.querySelectorAll("a[href^='#']");
+
+links.forEach(link => {
+    link.addEventListener("click", function (e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute("href"));
+        target.scrollIntoView({
+            behavior: "smooth"
+        });
+    });
+});
